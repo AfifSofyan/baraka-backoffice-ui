0526d272d2d908bc4dea145abf9df34689bebc27
Afif Fadhilah <101958500+AfifSofyan@users.noreply.github.com>
2025-09-09 17:22:01 +0800

    Update api-config.js
    rename api urls

--- src/config/api-config.js
+++ src/config/api-config.js
@@ -1,7 +1,8 @@
 const ApiEnvironment = {
     mock:'http://127.0.0.1:8080',
-    dev:"https://api-dev.barakaeducation.com",
-    prod:"https://api.barakaeducation.com"
+    dev:"https://api-dev.test.net",
+    prod:"https://api.test.net"
 }
+
 
 export const BASE_URL = ApiEnvironment.prod
